895a46dba2f0e7822fcd3c5e8b0186ced119fddf
IVAN LUKYANAU <sng.luka@gmail.com>
2020-01-04 04:01:11 +0300

    Added settings for test compose of docker file to check test of interaction between 2 services.

--- BookSeller.Tests/ValuesControllerTests.cs
+++ BookSeller.Tests/ValuesControllerTests.cs
@@ -1,4 +1,6 @@
-using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace BookSeller.Tests
@@ -16,6 +18,66 @@ namespace BookSeller.Tests
             var expected = 22;
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task Test_2()
+        {
+            var client = new HttpClient();
+
+            using (var response = await client.GetAsync("http://bookseller-api/api/values"))
+            {
+                Assert.True(response.IsSuccessStatusCode);
+
+                var respString = await response.Content.ReadAsStringAsync();
+                var respModel = JsonConvert.DeserializeObject<string[]>(respString);
+
+                Assert.NotNull(respModel);
+                Assert.NotEmpty(respModel);
+            }
+
+            using (var response = await client.GetAsync("http://bookwriter-api/api/values"))
+            {
+                Assert.True(response.IsSuccessStatusCode);
+
+                var respString = await response.Content.ReadAsStringAsync();
+                var respModel = JsonConvert.DeserializeObject<string[]>(respString);
+
+                Assert.NotNull(respModel);
+                Assert.NotEmpty(respModel);
+            }
+        }
+
+        [Fact]
+        public async Task Test_3()
+        {
+            var client = new HttpClient();
+            string[] respModel_Seller;
+            string[] respModel_Writer;
+            using (var response = await client.GetAsync("http://bookseller-api/api/values"))
+            {
+                Assert.True(response.IsSuccessStatusCode);
+
+                var respString = await response.Content.ReadAsStringAsync();
+                respModel_Seller = JsonConvert.DeserializeObject<string[]>(respString);
+
+                Assert.NotNull(respModel_Seller);
+                Assert.NotEmpty(respModel_Seller);
+            }
+
+            using (var response = await client.GetAsync("http://bookwriter-api/api/values"))
+            {
+                Assert.True(response.IsSuccessStatusCode);
+
+                var respString = await response.Content.ReadAsStringAsync();
+                respModel_Writer = JsonConvert.DeserializeObject<string[]>(respString);
+
+                Assert.NotNull(respModel_Writer);
+                Assert.NotEmpty(respModel_Writer);
+            }
+
+            Assert.True(respModel_Seller.Length == respModel_Writer.Length);
+            Assert.True(respModel_Seller[0].Equals(respModel_Writer[0]));
         }
     }
 }
